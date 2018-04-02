d5412791ce8f2c8c30e08570134c35f852b4e6af
rinafahridawati <rinafahridawati@yahoo.com>
2018-04-02 14:57:44 +0800

    mengisi nilai attribut

--- src/praktek02/kecepatanberaksi.java
+++ src/praktek02/kecepatanberaksi.java
@@ -2,6 +2,9 @@ package praktek02;
 public class kecepatanberaksi {
     public static void main(String[] args) {
         kecepatan kec1 = new kecepatan ();
+        kec1.jarak = 10;
+        kec1.waktu = 20;
+        
     }
     
 }
