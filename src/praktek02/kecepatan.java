4f536420e46efc4b65b556e13206074ab10ec239
rinafahridawati <rinafahridawati@yahoo.com>
2018-04-02 14:46:06 +0800

    membuat class baru

---
+++ src/praktek02/kecepatan.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek02;
+
+/**
+ *
+ * @author rina
+ */
+public class kecepatan {
+    
+}
